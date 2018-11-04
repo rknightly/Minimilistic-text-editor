31c7d9d95478367d2ef1d8f6b8a4b64fa6a02073
John Paul Antonovich <AntonovichJP@gmail.com>
2018-11-04 11:16:54 -0800

    Cleaned up main.py

--- src/main.py
+++ src/main.py
@@ -12,13 +12,18 @@ import tkinter.scrolledtext as scrollText
 commandKey = ''
 file_path = 'Untitled'
 
+# check platform the user is on
 if platform == 'darwin':
+    # if the user is on mac use Command for keyboard shorcuts
     commandKey = 'Command'
 else:
+    # otherwise use Control for keyboard shortcuts
     commandKey = 'Control'
 
 
 root = Tk()
+
+# settings for the scrolledText widget
 editArea = scrollText.ScrolledText(
     font=(
         str(editorSettings.fontName),
@@ -31,8 +36,7 @@ editArea.pack(expand=True, fill='both')
 
 root.title(file_path)
 
-
-
+# open a file
 def openFile(event=None):
     global editArea
     global file_path
@@ -48,30 +52,33 @@ def openFile(event=None):
     except:
         return
 
-
+# save to the current file
 def saveFile(event=None):
     global editArea
     global file_path
 
+    # if the user is not editing a file then create a new one
     if file_path == 'Untitled':
         saveAsFile()
+    # if the user is editing the settings file save to 'editorSettings.py'
     elif file_path == 'Settings':
         file = open('src/editorSettings.py', 'w')
         file.write(str(editArea.get(1.0, END)))
         file.close()
         root.title('Settings')
+    # otherwise save the file
     else:
         file = open(str(file_path), 'w')
         file.write(str(editArea.get(1.0, END)))
         file.close()
         root.title(file_path)
 
-
+# if tab is pressed enter the amount of space needed
 def insertTab(event=None):
     editArea.insert(tkinter.INSERT, " " * editorSettings.tabSize)
     return 'break'
 
-
+# create a new file
 def saveAsFile():
     global editArea
     global file_path
@@ -90,7 +97,7 @@ def saveAsFile():
         file_path = file
         root.title(file.name)
 
-
+# open the userSettings file
 def openSettingsFile():
     global editArea
     global file_path
@@ -106,14 +113,14 @@ def openSettingsFile():
     except:
         return
 
-
-
+# show a messagebox about the aplication
 def showAbout():
     messagebox.showinfo('About', 'Author: John Paul Antonovich\n\nLicense: MIT\n\nDescription: A simple text editor built with Python and the Tkinter library.')
 
 
 menubar = Menu(root)
 
+# create the 'file' menu
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label='Open  ' + commandKey + '-o', command=openFile)
 filemenu.add_command(label='Save  ' + commandKey + '-s', command=saveFile)
@@ -122,24 +129,27 @@ filemenu.add_separator()
 filemenu.add_command(label='Exit', command=root.quit)
 menubar.add_cascade(label='File', menu=filemenu)
 
-
+# create the 'settings menu'
 settingsmenu = Menu(menubar, tearoff=0)
 settingsmenu.add_command(label='Open Prefrences', command=openSettingsFile)
 menubar.add_cascade(label='Settings', menu=settingsmenu)
 
-
+# create the 'help menu'
 helpmenu = Menu(menubar, tearoff=0)
 helpmenu.add_command(label='About', command=showAbout)
 menubar.add_cascade(label='Help', menu=helpmenu)
 
+# add all of the menus to the window
 root.config(menu=menubar)
 
+# used for customized tabs
 editArea.bind('<Tab>', insertTab)
+
+# add keyboard shorcuts
 root.bind('<' + commandKey + '-s>', saveFile)
 root.bind('<' + commandKey + '-o>', openFile)
 root.bind('<' + commandKey + '-Shift-S>', saveAsFile)
 
 
 root.minsize(450, 450)
-
 root.mainloop()
